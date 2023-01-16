caebcc6fb90ee2c9d409e931acb3222d6184d9a4
Oleg <jadk.fre@Mail.ru>
2023-01-16 20:23:51 +0500

    start_dev

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
 import time
 import logging
-from . import config
+import config
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types,executor
 
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot=bot)
 
 
-@dp.message_handlers(commands=['/start'])
+@dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
     #Логируем
     user = message.from_user.id
     user_full_name = message.from_user.username
-    logging.INFO(f'{user}_{user_full_name}_{time.asctime()}')
+    logging.info(f'{user}_{user_full_name}_{time.asctime()}')
     #Возвращаем ответ
     await message.reply('Привет, ебать я работаю!')
+
