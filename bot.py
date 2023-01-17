3a68498eb67ac148ed489709c344a5ff424df427
Oleg <jadk.fre@Mail.ru>
2023-01-17 13:53:23 +0500

    start_dev

--- bot.py
+++ bot.py
@@ -4,14 +4,14 @@ import config
 import requests
 from aiogram import Bot, Dispatcher, types,executor
 
-bot = Bot(token=config.TOKEN)
+bot = Bot(token=config.TOKEN_API_TELEGRAM)
 dp = Dispatcher(bot=bot)
 
 
 #обработчик который обрабатывает команду start
 @dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
-    result = requests.get(f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={config.TOKEN_WEATHER}")
+    result = requests.get(f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={config.TOKEN_API_WEATHER}")
 
     await message.reply(result.text)
 
