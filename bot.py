fd59010b4b26a352f20632a16bc316cba3a2c65e
Oleg <jadk.fre@Mail.ru>
2023-01-17 13:50:17 +0500

    start_dev

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ dp = Dispatcher(bot=bot)
 #обработчик который обрабатывает команду start
 @dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
-    result = requests.get(f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={config.TOKEN_URL}")
+    result = requests.get(f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={config.TOKEN_WEATHER}")
 
     await message.reply(result.text)
 
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 #наш токен доступа к API телеграмма
 TOKEN = "5979613899:AAH4q-hJVyQjEua6v9aJToHQ-AnfD-hWU9g"
-TOKEN_URL = "6c06cce7d63081151f29f7af1fe4bf97"
+TOKEN_WEATHER = "6c06cce7d63081151f29f7af1fe4bf97"
+
