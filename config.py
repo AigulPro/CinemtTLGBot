aa319f445804e5b71daf057e8b1dc3f889dbe09a
Oleg <jadk.fre@Mail.ru>
2023-01-17 13:51:03 +0500

    start_dev

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-#наш токен доступа к API телеграмма
-TOKEN = "5979613899:AAH4q-hJVyQjEua6v9aJToHQ-AnfD-hWU9g"
-TOKEN_WEATHER = "6c06cce7d63081151f29f7af1fe4bf97"
-
+# наш токен доступа к API телеграмма
+TOKEN_API_TELEGRAM = "5979613899:AAH4q-hJVyQjEua6v9aJToHQ-AnfD-hWU9g"
+TOKEN_API_WEATHER = "6c06cce7d63081151f29f7af1fe4bf97"
